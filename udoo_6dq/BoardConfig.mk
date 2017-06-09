a333b2d9901637b57139e297cd31b0e580b083bd
secuflag <secuflag@ticino.com>
2017-06-09 16:43:16 +0200

    Remove UBI FS settings and cleanup EXT4 & F2FS definition

--- udoo_6dq/BoardConfig.mk
+++ udoo_6dq/BoardConfig.mk
@@ -5,44 +5,32 @@
 include device/udoo/imx6/soc/imx6dq.mk
 include device/udoo/udoo_6dq/build_id.mk
 include device/udoo/imx6/BoardConfigCommon.mk
+
 # udoo_6dq default target for EXT4
 BUILD_TARGET_FS ?= ext4
-include device/udoo/imx6/imx6_target_fs.mk
 
 ifeq ($(BUILD_TARGET_DEVICE),sd)
 ADDITIONAL_BUILD_PROPERTIES += \
-                        ro.internel.storage_size=/sys/block/mmcblk0/size \
-                        ro.boot.storage_type=sd \
-                        ro.frp.pst=/dev/block/mmcblk0p12
-ifneq ($(BUILD_TARGET_FS),f2fs)
-TARGET_RECOVERY_FSTAB = device/udoo/udoo_6dq/fstab_sd.freescale
-# build for ext4
-PRODUCT_COPY_FILES +=	\
-	device/udoo/udoo_6dq/fstab_sd.freescale:root/fstab.freescale
-else
-TARGET_RECOVERY_FSTAB = device/udoo/udoo_6dq/fstab_sd-f2fs.freescale
-# build for f2fs
-PRODUCT_COPY_FILES +=	\
-	device/udoo/udoo_6dq/fstab_sd-f2fs.freescale:root/fstab.freescale
-endif # BUILD_TARGET_FS
-else
-ADDITIONAL_BUILD_PROPERTIES += \
-                        ro.internel.storage_size=/sys/block/mmcblk0/size \
-                        ro.boot.storage_type=emmc \
-                        ro.frp.pst=/dev/block/mmcblk0p12
-ifneq ($(BUILD_TARGET_FS),f2fs)
-TARGET_RECOVERY_FSTAB = device/udoo/udoo_6dq/fstab.freescale
-# build for ext4
-PRODUCT_COPY_FILES +=	\
-	device/udoo/udoo_6dq/fstab.freescale:root/fstab.freescale
-else
-TARGET_RECOVERY_FSTAB = device/udoo/udoo_6dq/fstab-f2fs.freescale
-# build for f2fs
-PRODUCT_COPY_FILES +=	\
-	device/udoo/udoo_6dq/fstab-f2fs.freescale:root/fstab.freescale
-endif # BUILD_TARGET_FS
-endif # BUILD_TARGET_DEVICE
+  ro.internel.storage_size=/sys/block/mmcblk0/size \
+  ro.boot.storage_type=sd \
+  ro.frp.pst=/dev/block/mmcblk0p12
+endif
 
+# Build in EXT4
+ifeq ($(BUILD_TARGET_FS),ext4)
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_RECOVERY_FSTAB = device/udoo/udoo_6dq/fstab_ext4.freescale
+PRODUCT_COPY_FILES += \
+  device/udoo/udoo_6dq/fstab_ext4.freescale:root/fstab.freescale
+endif
+
+# Build in F2FS
+ifeq ($(BUILD_TARGET_FS),f2fs)
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_RECOVERY_FSTAB = device/udoo/udoo_6dq/fstab_f2fs.freescale
+PRODUCT_COPY_FILES += \
+  device/udoo/udoo_6dq/fstab_f2fs.freescale:root/fstab.freescale
+endif
 
 TARGET_BOOTLOADER_BOARD_NAME := udoo
 PRODUCT_MODEL := udoo-MX6DQ
@@ -93,30 +81,8 @@ TARGET_SELECT_KEY := 28
 # we don't support sparse image.
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 DM_VERITY_RUNTIME_CONFIG := true
-# uncomment below lins if use NAND
-#TARGET_USERIMAGES_USE_UBIFS = true
-
-
-ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
-UBI_ROOT_INI := device/udoo/udoo_6dq/ubi/ubinize.ini
-TARGET_MKUBIFS_ARGS := -m 4096 -e 516096 -c 4096 -x none
-TARGET_UBIRAW_ARGS := -m 4096 -p 512KiB $(UBI_ROOT_INI)
-endif
-
-ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
-ifeq ($(TARGET_USERIMAGES_USE_EXT4),true)
-$(error "TARGET_USERIMAGES_USE_UBIFS and TARGET_USERIMAGES_USE_EXT4 config open in same time, please only choose one target file system image")
-endif
-endif
 
 BOARD_KERNEL_CMDLINE := console=ttymxc1,115200 init=/init video=mxcfb0:dev=ldb,bpp=32 video=mxcfb1:off video=mxcfb2:off video=mxcfb3:off vmalloc=128M androidboot.console=ttymxc1 androidboot.dm_verity=disabled consoleblank=0 androidboot.hardware=freescale cma=448M
-
-ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
-#UBI boot command line.
-# Note: this NAND partition table must align with MFGTool's config.
-BOARD_KERNEL_CMDLINE +=  mtdparts=gpmi-nand:16m(bootloader),16m(bootimg),128m(recovery),-(root) gpmi_debug_init ubi.mtd=3
-endif
-
 
 # Broadcom BCM4339 BT
 BOARD_HAVE_BLUETOOTH_BCM := true
--- udoo_6sx/BoardConfig.mk
+++ udoo_6sx/BoardConfig.mk
@@ -5,32 +5,32 @@
 include device/udoo/imx6/soc/imx6sx.mk
 include device/udoo/udoo_6sx/build_id.mk
 include device/udoo/imx6/BoardConfigCommon.mk
+
 # udoo_6sx default target for EXT4
 BUILD_TARGET_FS ?= ext4
-include device/udoo/imx6/imx6_target_fs.mk
 
-ifeq ($(BUILD_TARGET_FS),ubifs)
-TARGET_RECOVERY_FSTAB = device/udoo/udoo_6sx/fstab_nand.freescale
-# build ubifs for nand devices
-PRODUCT_COPY_FILES +=	\
-	device/udoo/udoo_6sx/fstab_nand.freescale:root/fstab.freescale
-else
+ifeq ($(BUILD_TARGET_DEVICE),sd)
 ADDITIONAL_BUILD_PROPERTIES += \
-                        ro.internel.storage_size=/sys/block/mmcblk0/size \
-                        ro.frp.pst=/dev/block/mmcblk0p12
-ifneq ($(BUILD_TARGET_FS),f2fs)
-TARGET_RECOVERY_FSTAB = device/udoo/udoo_6sx/fstab.freescale
-# build for ext4
-PRODUCT_COPY_FILES +=	\
-	device/udoo/udoo_6sx/fstab.freescale:root/fstab.freescale
-else
-TARGET_RECOVERY_FSTAB = device/udoo/udoo_6sx/fstab-f2fs.freescale
-# build for f2fs
-PRODUCT_COPY_FILES +=	\
-	device/udoo/udoo_6sx/fstab-f2fs.freescale:root/fstab.freescale
-endif # BUILD_TARGET_FS
-endif # BUILD_TARGET_FS
+  ro.internel.storage_size=/sys/block/mmcblk0/size \
+  ro.boot.storage_type=sd \
+  ro.frp.pst=/dev/block/mmcblk0p12
+endif
 
+# Build in EXT4
+ifeq ($(BUILD_TARGET_FS),ext4)
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_RECOVERY_FSTAB = device/udoo/udoo_6sx/fstab_ext4.freescale
+PRODUCT_COPY_FILES += \
+  device/udoo/udoo_6sx/fstab_ext4.freescale:root/fstab.freescale
+endif
+
+# Build in F2FS
+ifeq ($(BUILD_TARGET_FS),f2fs)
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_RECOVERY_FSTAB = device/udoo/udoo_6sx/fstab_f2fs.freescale
+PRODUCT_COPY_FILES += \
+  device/udoo/udoo_6sx/fstab_f2fs.freescale:root/fstab.freescale
+endif
 
 TARGET_BOOTLOADER_BOARD_NAME := udoo
 PRODUCT_MODEL := udoo-MX6SX
@@ -82,30 +82,8 @@ TARGET_SELECT_KEY := 28
 # we don't support sparse image.
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 DM_VERITY_RUNTIME_CONFIG := true
-# uncomment below lins if use NAND
-#TARGET_USERIMAGES_USE_UBIFS = true
-
-
-ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
-UBI_ROOT_INI := device/udoo/udoo_6sx/ubi/ubinize.ini
-TARGET_MKUBIFS_ARGS := -m 4096 -e 516096 -c 4096 -x none
-TARGET_UBIRAW_ARGS := -m 4096 -p 512KiB $(UBI_ROOT_INI)
-endif
-
-ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
-ifeq ($(TARGET_USERIMAGES_USE_EXT4),true)
-$(error "TARGET_USERIMAGES_USE_UBIFS and TARGET_USERIMAGES_USE_EXT4 config open in same time, please only choose one target file system image")
-endif
-endif
 
 BOARD_KERNEL_CMDLINE := console=ttymxc1,115200 init=/init androidboot.console=ttymxc1 androidboot.dm_verity=disabled consoleblank=0 androidboot.hardware=freescale vmalloc=128M cma=448M
-
-ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
-#UBI boot command line.
-# Note: this NAND partition table must align with MFGTool's config.
-BOARD_KERNEL_CMDLINE +=  mtdparts=gpmi-nand:16m(bootloader),16m(bootimg),128m(recovery),-(root) gpmi_debug_init ubi.mtd=3
-endif
-
 
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/udoo/udoo_6sx/bluetooth
