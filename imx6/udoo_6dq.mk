39b01903e46ffebdc2a9c7d8f44c1fbca36723c8
secuflag <secuflag@ticino.com>
2017-06-13 17:01:40 +0200

    Move multiple initrc and audio policy files inside their own directories

--- imx6/udoo_6dq.mk
+++ imx6/udoo_6dq.mk
@@ -17,22 +17,24 @@ PRODUCT_NAME := udoo_6dq
 PRODUCT_DEVICE := udoo_6dq
 PRODUCT_MODEL := UDOO-MX6DQ
 
+# initrc files
 PRODUCT_COPY_FILES += \
-	device/udoo/udoo_6dq/init.rc:root/init.freescale.rc \
-        device/udoo/udoo_6dq/init.i.MX6Q.rc:root/init.freescale.i.MX6Q.rc \
-        device/udoo/udoo_6dq/init.i.MX6DL.rc:root/init.freescale.i.MX6DL.rc \
-	device/udoo/udoo_6dq/init.i.MX6QP.rc:root/init.freescale.i.MX6QP.rc \
+    device/udoo/udoo_6dq/initrc/init.rc:root/init.freescale.rc \
+    device/udoo/udoo_6dq/initrc/init.i.MX6Q.rc:root/init.freescale.i.MX6Q.rc \
+    device/udoo/udoo_6dq/initrc/init.i.MX6DL.rc:root/init.freescale.i.MX6DL.rc \
+    device/udoo/udoo_6dq/initrc/init.i.MX6QP.rc:root/init.freescale.i.MX6QP.rc
 
 # Audio
 USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_COPY_FILES += \
-	device/udoo/udoo_6dq/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-	device/udoo/udoo_6dq/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
-	frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
+    device/udoo/udoo_6dq/audio/audio_effects.conf:system/etc/audio_effects.conf \
+    device/udoo/udoo_6dq/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    device/udoo/udoo_6dq/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml
 
 PRODUCT_COPY_FILES +=	\
 	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6d.bin:system/lib/firmware/vpu/vpu_fw_imx6d.bin 	\
--- imx6/udoo_6sx.mk
+++ imx6/udoo_6sx.mk
@@ -17,19 +17,21 @@ PRODUCT_NAME := udoo_6sx
 PRODUCT_DEVICE := udoo_6sx
 PRODUCT_MODEL := UDOO-MX6SX
 
+# initrc
 PRODUCT_COPY_FILES += \
 	device/udoo/udoo_6sx/init.rc:root/init.freescale.rc \
 
 # Audio
 USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_COPY_FILES += \
-	device/udoo/udoo_6sx/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-	device/udoo/udoo_6sx/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
-	frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
+    device/udoo/udoo_6sx/audio_effects.conf:system/etc/audio_effects.conf \
+    device/udoo/udoo_6sx/audio_policy.conf:system/etc/audio_policy.conf \
+    device/udoo/udoo_6sx/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
 
 # GPU files
 
